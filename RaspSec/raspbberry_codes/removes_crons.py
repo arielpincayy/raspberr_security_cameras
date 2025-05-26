28d84a643a768da3996d943fa7aad8582326d16a
J0sephT <119791550+J0sephT@users.noreply.github.com>
2025-05-26 11:32:13 -0500

    Subiendo carpeta RaspSec limpia con la página web

--- RaspSec/raspbberry_codes/removes_crons.py
+++
@@ -1,6 +0,0 @@
-from crontab import CronTab
-
-cron = CronTab(user=True)
-cron.remove_all(comment='captura_raspberry')
-cron.write()
-print("❌ Todas las tareas de captura eliminadas.")
--- RaspSec/server/config.py
+++
@@ -1,6 +0,0 @@
-DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'arieluser',
-    'password': 'miclave123',
-    'database': 'proyecto_multimedia'
-}
